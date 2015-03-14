45cf99a5f6a73dc4b6c480f821deb50579e70ffd
apinballwizard <sapinballwizard@gmail.com>
2015-03-14 08:33:51 -0500

    Update yuspos.user.js

--- yuspos.user.js
+++ yuspos.user.js
@@ -100,10 +100,10 @@ for (var i=0; i < webmLinks.length; i++) {
 //	if(warn == false){ window.location.href = 'http://forums.somethingawful.com/forumdisplay.php?forumid=219' };
 //};
 
-if (jQuery('.userinfo.userid-185872').size() > 0) { 
+/*if (jQuery('.userinfo.userid-185872').size() > 0) { 
     loadScript("http://code.onion.com/fartscroll.js", function () { fartscroll(500); console.log("fartscroll activated");} );
     console.log("fartscroll set");
-}
+}*/
 
 //resize images like the forums damn well should - TODO: auto timg style but as wide as screen??
 //firefox is a garbage and doesnt understand max-width:100% inside tables
